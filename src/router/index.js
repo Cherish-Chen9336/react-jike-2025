3c32c3531d3aea04bf48cff64a26b6f60e018dca
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-03-27 21:47:29 +0800

    token控制路由权限

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import { createBrowserRouter } from 'react-router-dom'
+import { AuthRoute } from '@/components/AuthRoute'
 
+// 配置路由实例
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: (
+      <AuthRoute>
+        <Layout />
+      </AuthRoute>
+    ),
   },
   {
     path: '/Login',
