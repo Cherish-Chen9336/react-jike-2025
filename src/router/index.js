275040b1186554d6748f874ec89f3dc29b3d04d2
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-03-27 18:10:31 +0800

    项目初始化环境配置

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+//配置页面  路由
+import Layout from '@/pages/Layout'
+import Login from '@/pages/Login'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+  },
+  {
+    path: '/Login',
+    element: <Login />,
+  },
+])
+
+export default router
